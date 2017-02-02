d93c1a6ee037d75a718800f3e0dd82268815fe10
Kai Neuwerth <kai.neuwerth@netshops.de>
2017-02-02 12:18:18 +0100

    linked echos in entrypoint.sh to /dev/stdout

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,40 +2,38 @@
 
 # Prechecks
 if [ -z "$SHOPWARE_RELEASE_URL" ] ; then
-	echo "Variable SHOPWARE_RELEASE_URL is not set. Exiting."
+	echo "Variable SHOPWARE_RELEASE_URL is not set. Exiting." > /dev/stdout
 	exit 1
 fi
 
 if [ -z "$EXTRACT_PATH" ] ; then
-	echo "Variable EXTRACT_PATH is not set. Exiting."
+	echo "Variable EXTRACT_PATH is not set. Exiting." > /dev/stdout
 	exit 1
 fi
 
 # Start workflow
-echo "Cleaning up $EXTRACT_PATH..."
+echo "Cleaning up $EXTRACT_PATH..." > /dev/stdout
 rm -rf $EXTRACT_PATH/*
-echo "Done."
+echo "Done." > /dev/stdout
 
 cd /tmp
 
 
 if [ ! -f "shopware.zip" ] ; then
-	echo "Downloading Shopware release from $SHOPWARE_RELEASE_URL."
+	echo "Downloading Shopware release from $SHOPWARE_RELEASE_URL." > /dev/stdout
 	wget -q $SHOPWARE_RELEASE_URL -O shopware.zip
 fi
 
-echo "Extracting Shopware release to $EXTRACT_PATH."
+echo "Extracting Shopware release to $EXTRACT_PATH." > /dev/stdout
 unzip -qq -u shopware.zip -d $EXTRACT_PATH
 
-echo "Setting correct chmods in $EXTRACT_PATH."
+echo "Setting correct chmods in $EXTRACT_PATH." > /dev/stdout
 cd $EXTRACT_PATH
 chmod 777 config.php
-chmod 777 -R cache/
 chmod 777 -R custom/
 chmod 777 -R files/
 chmod 777 -R media/
 chmod 777 -R engine/Shopware/Plugins/Community/
-chmod 777 -R logs/
 chmod 777 -R engine/Shopware/Plugins/Default/
 chmod 777 -R web/cache/
 chmod 777 -R recovery/
@@ -48,8 +46,8 @@ if [ ! "$FULL_WIPE_ON_REBOOT" = "true" ] ; then
 else
     for i in {1..10}
     do
-       echo "!!! WARNING: Env variable FULL_WIPE_ON_REBOOT is set!!!"
+       echo "!!! WARNING: Env variable FULL_WIPE_ON_REBOOT is set!!!" > /dev/stdout
     done
 fi
 
-echo "Done."
+echo "Done." > /dev/stdout
